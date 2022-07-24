dd7f03817601bccf38a54db4f534f959940de3ec
OlgaRadkovska <oradkovskaya@gmail.com>
2022-07-24 14:18:21 +0300

    Weather forecast added

--- script.js
+++ script.js
@@ -36,6 +36,21 @@ let currentCity = document.querySelector("h1").innerText;
 form.addEventListener("submit", queryCity);
 let iconElement = document.querySelector("#icon");
 
+function formatWeekDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+
+  return days[day];
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "0efb4fc16a9ed98dc0b3aafd8491d6ad";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayCurrentWeather(response) {
   let temperatureElement = document.querySelector("#default-temp");
   document.querySelector("#city").innerHTML = response.data.name;
@@ -55,6 +70,52 @@ function displayCurrentWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].main);
+
+  getForecast(response.data.coord);
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  // let forecastHTML = `<div class = "row">`
+  let forecastHTML = "";
+  console.log(forecastHTML);
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 7) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="card-container">
+      <div class="card-header weather-forecast-date">${formatWeekDay(
+        forecastDay.dt
+      )}</div>
+    <div class="card-body">
+      <h5 class="card-title">
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          width="50px"
+          alt="sunny"
+          id="icon-01"
+        />
+      </h5>
+      <p class="card-text weather-forecast-temperature">
+        <span class="weather-forecast-temperature-max">${Math.round(
+          forecastDay.temp.max
+        )}&deg;C</span>
+        <span class="weather-forecast-temperature-min"> ${Math.round(
+          forecastDay.temp.min
+        )}&deg;C</span>
+      </p>
+    </div>
+    </div>`;
+    }
+  });
+
+  // forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function search(city) {
@@ -112,20 +173,3 @@ let celsiuslink = document.querySelector("#celsius-link");
 celsiuslink.addEventListener("click", displayCelsiusTemperature);
 
 search("Kyiv");
-
-// let fahrenheitSwitcher = document.querySelector("#fahrenheit-swither");
-// fahrenheitSwitcher.addEventListener("click", calcFahrenheit);
-
-// function calcFahrenheit(event) {
-//   event.preventDefault();
-//   let fahrenheitTemp = defaulTempValue * 1.8 + 32;
-//   defaulTemp.innerHTML = fahrenheitTemp;
-// }
-
-// function calcCelsium() {
-//   defaulTemp.innerHTML = defaulTempValue;
-// }
-
-// // fahrenheitTemp.addEventListener("click", calcFahrenheit);
-
-// celciumTemp.addEventListener("click", calcCelsium);
